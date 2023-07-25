a04215b6fbea58f61ddaf93f75d9a69da15cc1f6
mustafa99 <mustafayevmustafa99@gmail.com>
2023-07-25 15:25:17 +0400

    dockersss

--- permission.sh
+++ permission.sh
@@ -1,5 +1,5 @@
-chown -R laravel:nginx  . \
+chown -R nginx:nginx  . \
 && find . -type f -exec chmod 644 {} \; \
 && find . -type d -exec chmod 755 {} \; \
-&& chgrp -R laravel storage bootstrap/cache \
+&& chgrp -R nginx storage bootstrap/cache \
 && chmod -R ug+rwx storage bootstrap/cache
