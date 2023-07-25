a13ae39593f3d10a60a8717b8e04acadded26b05
mustafa99 <mustafayevmustafa99@gmail.com>
2023-07-25 12:01:04 +0400

    docker

--- permission.sh
+++ permission.sh
@@ -1,4 +1,4 @@
-sudo chown -R laravel:laravel . \
+chown -R laravel:laravel . \
 && find . -type f -exec chmod 644 {} \; \
 && find . -type d -exec chmod 755 {} \; \
 && chgrp -R laravel storage bootstrap/cache \
